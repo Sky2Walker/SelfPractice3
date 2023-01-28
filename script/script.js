6f7c98b9f4b1a7ab44f1601b3d858ce23dbe09ce
Sky2Walker <starw8169@gmail.com>
2023-01-28 21:16:48 +0200

    slide

--- script/script.js
+++ script/script.js
@@ -7,3 +7,24 @@ function openSearch() {
 function closeSearch() {
     document.getElementById("myOverlay").style.display = "none";
 }
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+    slidesPerView:3,
+
+    spaceBetween:40,
+
+});
