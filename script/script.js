4dfc107e49800297e1c90a0fcac7c4c8b607bcd1
Sky2Walker <starw8169@gmail.com>
2023-01-31 22:17:20 +0200

    pre footer

--- script/script.js
+++ script/script.js
@@ -28,3 +28,27 @@ const swiper = new Swiper('.swiper', {
     spaceBetween:40,
 
 });
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".set > a").on("click", function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass("active");
+            $(this).siblings('.content').slideUp(200);
+            $(".set > a span").removeClass("minus").addClass("plus");
+        }else{
+            $(".set > a span").removeClass("minus").addClass("plus");
+            $(this).find("span").removeClass("plus").addClass("minus");
+            $(".set > a").removeClass("active");
+            $(this).addClass("active");
+            $('.content').slideUp(200);
+            $(this).siblings('.content').slideDown(200);
+        }
+    });
+});
